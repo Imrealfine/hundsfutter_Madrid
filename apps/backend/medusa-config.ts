4c06b6d97c47411dd9cea7ee9a40b86912fddb7d
Dong Xu <161396564+Imrealfine@users.noreply.github.com>
2026-09-25 15:10:09 +0200

    Update medusa-config with Redis URL and admin settings

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -1,16 +1,43 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+
+    databaseDriverOptions: {
+      ssl: false,
+      sslmode: "disable",
+    },
+
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
-    }
-  }
+    },
+  },
+
+  admin: {
+    vite: (config) => {
+      return {
+        ...config,
+        server: {
+          ...config.server,
+          host: "0.0.0.0",
+
+          // 仅适合你现在 NAS 局域网开发
+          allowedHosts: true,
+
+          hmr: {
+            port: 5173,
+            clientPort: 5173,
+          },
+        },
+      }
+    },
+  },
 })
